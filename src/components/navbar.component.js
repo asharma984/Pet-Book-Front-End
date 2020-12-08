d1e08e607eb53db54e9614a2620084c5ea78fab4
David Ryan <ryan.davi@husky.neu.edu>
2020-12-08 10:15:33 -0500

    testing navbar
    Added testing navbar, probably can remove later

---
+++ src/components/navbar.component.js
@@ -0,0 +1,24 @@
+
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Navbar extends Component{
+
+    render(){
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">PetBlogApp</Link>
+                <div className="collpase navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="navbar-item">
+                        <Link to="/" className="nav-link">Account</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="/AdvancedSearchComponent" className="nav-link">Search</Link>
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        )
+    }
+}
