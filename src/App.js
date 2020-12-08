5a97c7c5e2fbe2e65203e02cd8711f32e730d170
David Ryan <ryan.davi@husky.neu.edu>
2020-12-08 10:16:02 -0500

    updated app.js
    updated app.js to work with testing env routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Link, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 // import components
-import AdvancedSearchComponent from './components/AdvancedSearchComponent';
-import PetGridComponent from './components/PetGridComponent';
-import PetContainer from './components/PetContainer'
-import PetProfileComponent from './components/PetProfileComponent'
+import Navbar from "./components/navbar.component";
+import Account from "./containers/account.container";
+import PetProfile from "./containers/pet-profile.container";
+import CreateBlogPost from "./components/create-blogpost.component";
+import CreatePet from "./components/create-pet.component";
+import AdvancedSearchComponent from "./components/AdvancedSearchComponent";
+import PetGridComponent from "./components/PetGridComponent";
+import PetContainer from "./components/PetContainer";
 
 class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-
                 <div className="App container-fluid">
-                    {/* We are using a link here so we can get back to the main page whenever we
-         click this. Probably want to update this to the user's homepage when we go live */}
-                    <h1><Link to="/">Pet Search!</Link></h1>
-                    {/* we are loading the AdvancedSearchComponent
-                     since there's no 'landing page' in this incarnation */}
-                    <AdvancedSearchComponent/>
-
-                    {/* Routes for Navigation*/}
-                    <Route path="/AdvancedSearchComponent" exact
-                           component={AdvancedSearchComponent}/>
+                    <Navbar/>
+                    <br/>
+                    <Route path="/" exact component={Account}/>
+                    <Route path="/profile/:petId" exact component={PetProfile}/>
+                    <Route path="/blog/:petId" exact component={CreateBlogPost}/>
+                    <Route path="/pet/:userId" exact component={CreatePet}/>
+                    <Route path="/AdvancedSearchComponent" exact component={AdvancedSearchComponent}/>
                     <Route path={["/PetGridComponent/search"]} exact component={PetGridComponent}/>
-                    <Route path="/PetGridComponent/:animalType/:animalId" exact
-                           component={PetContainer}/>
-                    <Route path="/user/:userId/pet/:petId" exact
-                           component={PetProfileComponent}/>
-
+                    <Route path="/PetGridComponent/:animalType/:animalId" exact component={PetContainer}/>
                 </div>
             </BrowserRouter>
         );
