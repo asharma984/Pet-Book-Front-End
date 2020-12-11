2b4a9b7218ae3a549a993d9bcaec7953977e7978
asharma984 <aditya.sharma984@gmail.com>
2020-12-11 15:47:31 -0500

    added user type

--- src/components/HomeComponents/Home.js
+++ src/components/HomeComponents/Home.js
@@ -8,19 +8,33 @@ export default function Home(){
     const {userData}=useContext(UserContext);
     useEffect(()=>{
        if(!userData.user) history.replace("/login");
+       if(userData.user) {
+           console.log(userData.user.type);
+           console.log(userData.user.id);
+       }
     });
     const submit=async (e)=>{
         history.replace("/");
     }
     return(
         <div className="page">
-
+            { userData.user && userData.user.type==="pet-owner" &&
             <form className="form" onSubmit={submit}>
                 <input type="submit" value="Add Pets" />
+
                 <input type="submit" value="Your Pets" />
                 <input type="submit" value="Pets you follow" />
 
-            </form>
+            </form> }
+
+            { userData.user && userData.user.type==="pet-shelter" &&
+              <form className="form" onSubmit={submit}>
+                  <input type="submit" value="Add Pets" />
+
+                  <input type="submit" value="Your Pets" />
+
+
+              </form> }
         </div>
     )
 }
