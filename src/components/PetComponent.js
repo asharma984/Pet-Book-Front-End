e2bdc90ea14988e10bf8441f8b35575a0be595dc
David Ryan <ryan.davi@husky.neu.edu>
2020-12-13 16:33:26 -0500

    Update PetComponent.js
    Set to local server to test with

--- src/components/PetComponent.js
+++ src/components/PetComponent.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
+import React, {useState, useContext,} from 'react';
+import UserContext from "../contex/UserContext";
 
-// This is almost a repeat of the PetComponent, but will be used for rendering profile.
+const serverURL = 'http://localhost:5000';
 
-class PetComponent extends React.Component {
-  render() {
-    const animal = this.props;
+function PetComponent(props) {
+    const history = useHistory();
+    const {userData} = useContext(UserContext);
+    const animal = props;
     const animalType = animal.animalType;
     const name = animal.name;
     const age = animal.age;
@@ -15,63 +17,105 @@ class PetComponent extends React.Component {
     const state = animal.location.state;
     const userId = animal.userId;
     let pictures =
-      'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg'; // image not found
+        'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg'; // image not found
+
+    const isOwner = userData.user && userData.user.id === props.userId
 
     // check if there are pictures
     if (animal.pictures[0]) {
-      // check if the pictures are from us or petfinder
-      if (!animal.pictures[0]['full']) {
-        pictures = animal.pictures[0];
-      } else {
-        pictures = animal.pictures[0]['full'];
-      }
+        // check if the pictures are from us or petfinder
+        if (!animal.pictures[0]['full']) {
+            pictures = animal.pictures[0];
+        } else {
+            pictures = animal.pictures[0]['full'];
+        }
     }
-    return (
-      <div className="col">
-        {/* display this info if it's a pet on our site
-                Old link/david route link                 <Link to={`/profile/${animalId}`}
-                */}
-        {userId && (
-          <Link
-            to={`/user/${userId}/profile/${animalId}`}
-            className="card headerBackgroundColor"
-            style={{ padding: '1rem', margin: '1rem' }}
-          >
-            <img className="card-img-top" src={pictures} alt={name} />
-            <div className="card-body tableRowHighlight">
-              <h5 className="card-title">{name}</h5>
-              <p className="card-text">
-                {age} {breed}
-              </p>
-              <p className="card-text">
-                {city}, {state}
-              </p>
-            </div>
-          </Link>
-        )}
 
-        {/* display this info if it's an outside our site pet */}
-        {!userId && (
+    return (
+        <div className="col">
+            {!isOwner && userId &&
+             <span className="card headerBackgroundColor"
+                   style={{padding: '1rem', margin: '1rem'}}>
           <Link
-            to={`/PetGridComponent/${animalType}/${animalId}`}
-            className="card headerBackgroundColor"
-            style={{ padding: '1rem', margin: '1rem' }}
+              to={`/user/${userId}/profile/${animalId}`}
           >
-            <img className="card-img-top" src={pictures} alt={name} />
+            <img className="card-img-top" src={pictures} alt={name}/>
+          </Link>
             <div className="card-body tableRowHighlight">
-              <h5 className="card-title">{name}</h5>
-              <p className="card-text">
+                {props.isFollowed &&
+                 <h5 className="card-title">{name}
+                     <button className="btn"
+                             onClick={() => {
+
+                             }}>Unfollow Me
+                     </button>
+                 </h5>
+                }
+
+                {!props.isFollowed &&
+                 <h5 className="card-title">{name}
+                     <button className="btn"
+                             onClick={() => {
+
+                             }}>Follow Me!
+                     </button>
+                 </h5>
+                }
+                <p className="card-text">
                 {age} {breed}
               </p>
               <p className="card-text">
                 {city}, {state}
               </p>
             </div>
+            </span>
+            }
+
+            {isOwner  &&
+             <span className="card headerBackgroundColor"
+                   style={{padding: '1rem', margin: '1rem'}}>
+          <Link
+              to={`/user/${userId}/profile/${animalId}`}
+          >
+            <img className="card-img-top" src={pictures} alt={name}/>
           </Link>
-        )}
-      </div>
+            <div className="card-body tableRowHighlight">
+                 <h5 className="card-title">{name}
+                 </h5>
+                <p className="card-text">
+                {age} {breed}
+              </p>
+              <p className="card-text">
+                {city}, {state}
+              </p>
+            </div>
+            </span>
+            }
+
+            {/* display this info if it's an outside our site pet */}
+            {!userId &&
+             <span className="card headerBackgroundColor"
+                   style={{padding: '1rem', margin: '1rem'}}>
+                                  <Link
+                                      to={`/PetGridComponent/${animalType}/${animalId}`}
+
+                                  >
+                    <img className="card-img-top" src={pictures} alt={name}/>
+                                    </Link>
+                    <div className="card-body tableRowHighlight">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">
+                            {age} {breed}
+                        </p>
+                        <p className="card-text">
+                            {city}, {state}
+                        </p>
+                    </div>
+
+                </span>
+            }
+        </div>
     );
-  }
 }
 
 export default PetComponent;
