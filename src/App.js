305a2a95e076c61e3537217d4a9aad2a11571777
David Ryan <ryan.davi@husky.neu.edu>
2020-12-13 16:32:21 -0500

    removed unused paths
    removed unused paths

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 // import components
 import Account from './containers/account.container';
-import PetProfile from './containers/pet-profile.container';
-import CreateBlogPost from './components/create-blogpost.component';
-import CreatePet from './components/create-pet.component';
 import AdvancedSearchComponent from './components/AdvancedSearchComponent';
 import PetGridComponent from './components/PetGridComponent';
 import PetContainer from './components/PetContainer';
@@ -18,13 +15,6 @@ class App extends React.Component {
         <div className="App container-fluid">
           <br />
           <Route path="/" exact component={Account} />
-          <Route
-            path="user/:userId/profile/:petId"
-            exact
-            component={PetProfile}
-          />
-          <Route path="/blog/:petId" exact component={CreateBlogPost} />
-          <Route path="/pet/:userId" exact component={CreatePet} />
           <Route
             path="/AdvancedSearchComponent"
             exact
